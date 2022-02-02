48216eec42d96a30e9661823c012a01f6f7f117d
Yitz <chalfari@gmail.com>
2022-02-02 10:05:14 +0200

    add time stamps and helper funcs

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,44 +1,78 @@
 const socket = io();
-const messageInput = document.querySelector("message");
+const $messageForm = document.querySelector("#message-form");
+const messageInput = $messageForm.querySelector("input");
 const messageBox = document.querySelector(".message-box");
-const messageForm = document.querySelector("#message-form");
-
-messageForm.addEventListener("submit", (e) => {
+const $messageFormButton = $messageForm.querySelector("button");
+const $locationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+//templates Mustache
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  //disable form until after message was sent
+  $messageFormButton.setAttribute("disabled", "disabled");
   //we can target the input name from the forms e.target.elements
   const message = e.target.elements.message.value;
   //The data being emitted could be as many arguments as we want (not only message)
   //emit can take a callback as the last argument
   //if we want to recieve something upon being acknowledged we add a parameter
-  socket.emit("sendMessage", message, (m) => {
-    //this runs when even has been acknowledged (received by the listener)
-    console.log("The message was delivered!", m);
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled", "disabled");
+    messageInput.value = "";
+    messageInput.focus();
+    //this runs when event has been acknowledged (received by the listener)
+    if (error) {
+      return console.log(error);
+    }
+    console.log("message delivered");
   });
-
-  messageForm.reset();
+  // $messageForm.reset();
 });
-
 socket.on("sendMessage", (m) => {
-  messageBox.innerHTML += `<p>${m}</p>`;
-  console.log(m);
+  // messageBox.innerHTML += `<p>${m}</p>`;
+  // console.log(m);
+  const html = Mustache.render(messageTemplate, {
+    message: m.text,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
 socket.on("message", (m) => {
-  console.log(m);
-  messageBox.innerHTML += `<p>${m}</p>`;
+  const html = Mustache.render(messageTemplate, {
+    message: m.text,
+    createdAt: moment(m.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
 });
-
 socket.on("welcome", (welcome) => {
   console.log(welcome);
 });
+socket.on("locationMessage", (message) => {
+  const html = Mustache.render(locationMessageTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format(""),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$locationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("your browser does not support geolocation");
   }
+  $locationButton.setAttribute("disabled", "disabled");
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit("sendLocation", {
-      lat: position.coords.latitude,
-      long: position.coords.longitude,
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      },
+      (res) => {
+        $locationButton.removeAttribute("disabled", "disabled");
+        console.log(res);
+      }
+    );
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-
+const Filter = require("bad-words");
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
 //socket.io needs us to pass in http.createServer explicitly so we need to import it manually
 //even though express does it automatically
 const app = express();
@@ -16,27 +20,40 @@ app.use(express.static(publicDirectory));
 
 io.on("connection", (socket) => {
   console.log("new connection");
-  socket.emit("welcome", "Welcome!");
+  socket.emit("message", generateMessage("Welcome!"));
   //broadcast to all besides for the user (socket) who connected
-  socket.broadcast.emit("message", "A new user has joined");
+  socket.broadcast.emit("message", generateMessage("A new user has joined"));
   //the callback is to acknowledge the event(notify client that the message was recieved)
   //this callback is from the 3rd param of the emitter
+  //only the emitter recieves the acknowledgement
   socket.on("sendMessage", (m, callback) => {
-    io.emit("sendMessage", m);
+    // innitialize bad-words
+    const filter = new Filter();
+    if (filter.isProfane(m)) {
+      return callback("Profanity is not allowed");
+    }
+    io.emit("sendMessage", generateMessage(m));
     //the callback can be empty or can put data to deliver
-    callback("Delivered");
+    callback();
   });
   //listen and broadcast location
-  socket.on("sendLocation", (pos) => {
-    console.log("hello?");
-    io.emit("message", `https://www.google.com/maps/@${pos.lat},${pos.long}`);
+  socket.on("sendLocation", (pos, acknowledgeCallback) => {
+    io.emit(
+      "locationMessage",
+      generateLocationMessage(
+        `https://www.google.com/maps/@${pos.lat},${pos.long}`
+      )
+    );
+    acknowledgeCallback("Positions delivered");
   });
   //to listen to disconnections we don't use io.on
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left");
+    io.emit("message", generateMessage("A user has left"));
   });
 });
 
 server.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+//get time function
